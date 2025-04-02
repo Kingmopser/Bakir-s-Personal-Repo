86601ff570d70764b2243b069d852d59ef3ee791
Bakir Chaban <B.Chaban@campus.lmu.de>
2025-04-02 23:00:50 +0200

    update

---
+++ src/main.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import root_mean_squared_error
